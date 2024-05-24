d7c0a78dc48bf20b571716dc0dec7e71e7f3be90
Nifemi-git <nifemiakingba@gmail.com>
2024-05-24 20:00:42 +0100

    add mobile and active styles, add eye icon

--- app.js
+++ app.js
@@ -1,55 +1,83 @@
-const form = document.getElementById('form');
-const button = document.getElementById('button');
-const firstName = document.querySelector('.firstName');
-const lastName = document.querySelector('.lastName');
-const email = document.querySelector('.email');
-const password = document.querySelector('.password');
+const form = document.getElementById("form");
+const button = document.getElementById("button");
+const firstName = document.querySelector(".firstName");
+const firstNameErrMsg = document.getElementById("error-msg-firstname");
+const lastName = document.querySelector(".lastName");
+const lastNameErrMsg = document.getElementById("error-msg-lastname");
+const email = document.querySelector(".email");
+const emailErrMsg = document.getElementById("error-msg-email");
+const passwordLabel = document.querySelector(".password-label");
+const password = document.querySelector(".password");
+const eyeIcon = document.getElementById("eye-icon");
+const passwordErrMsg = document.getElementById("error-msg-password");
 
-console.log(firstName);
-
-// console.log(firstName, lastName, email, password);
-
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   const fName = firstName.value;
   const lName = lastName.value;
   const emailVal = email.value;
   const passwordVal = password.value;
-  console.log(fName, lName, emailVal, passwordVal);
 
   // Check first name
-  if (fName === '') {
-    firstName.classList.add('error');
+  if (fName === "") {
+    firstName.classList.add("error", "error-bg");
+    firstNameErrMsg.style.display = "block";
   } else {
-    firstName.classList.remove('error');
+    firstName.classList.remove("error", "error-bg");
+    firstNameErrMsg.style.display = "none";
   }
   // Check last name
 
-  if (lName === '') {
-    lastName.classList.add('error');
+  if (lName === "") {
+    lastName.classList.add("error", "error-bg");
+    lastNameErrMsg.style.display = "block";
   } else {
-    lastName.classList.remove('error');
+    lastName.classList.remove("error", "error-bg");
+    lastNameErrMsg.style.display = "none";
   }
   // Check email
 
-  if (!validateEmail(emailVal) || emailVal === '') {
-    email.classList.add('error');
+  if (emailVal === "") {
+    email.classList.add("error", "error-bg");
+    emailErrMsg.textContent = "Email cannot be empty";
+    emailErrMsg.style.display = "block";
+  } else if (!validateEmail(emailVal)) {
+    email.classList.add("error", "error-bg");
+    email.style.color = "red";
+    emailErrMsg.textContent = "Looks like this is not an email";
+    emailErrMsg.style.display = "block";
   } else {
-    email.classList.remove('error');
+    email.classList.remove("error", "error-bg");
+    email.style.color = "black";
+    emailErrMsg.style.display = "none";
   }
 
   // Check password
 
-  if (passwordVal === '') {
-    password.classList.add('error');
+  if (passwordVal === "") {
+    passwordLabel.classList.add("error");
+    passwordErrMsg.style.display = "block";
   } else {
-    password.classList.remove('error');
+    passwordLabel.classList.remove("error");
+    passwordErrMsg.style.display = "none";
   }
 });
 
 //Validate email
 function validateEmail(email) {
-  var re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var re = /^[a-z0-9]+@[a-z]+\.com$/;
+  // /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+
+const togglePasswordVisibility = () => {
+  if (password.getAttribute("type") === "password") {
+    password.setAttribute("type", "text");
+    eyeIcon.setAttribute("src", "images/eye-open.svg");
+  } else {
+    password.setAttribute("type", "password");
+    eyeIcon.setAttribute("src", "images/eye-slash.svg");
+  }
+};
+
+eyeIcon.onclick = togglePasswordVisibility;
